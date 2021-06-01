2df06c42f5be5759ccce8727a666d3c3fd11112f
chaudharparth <parthc3233@gmail.com>
2021-06-01 16:53:52 +0530

    Model Declarations

---
+++ src/main/java/com/apps/common/StringCalculator.java
@@ -0,0 +1,16 @@
+package com.apps.common;
+
+/**
+ * Class for handling string addition
+ * @author parth
+ *
+ */
+public class StringCalculator {
+	public StringCalculator() {
+		
+	}
+	
+	public int Add(String numbers) {
+		return 0;
+	}
+}
---
+++ src/test/java/com/apps/common/StringCalculatorTest.java
@@ -0,0 +1,14 @@
+/**
+ * Class for handling test : string addition
+ * @author parth
+ *
+ */
+
+package com.apps.common;
+
+public class StringCalculatorTest {
+	
+	public void AddTest() {
+		
+	}
+}
